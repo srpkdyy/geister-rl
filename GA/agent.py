da2a1709dabb572c7ea755e88a0aa6750d7aac9d
srpkdyy <srpkdyy@gmail.com>
2021-12-14 12:34:48 +0900

    update GAgent

--- GA/agent.py
+++ GA/agent.py
@@ -1,5 +1,4 @@
-import os
-import sys
+import os, sys
 sys.path.append(os.pardir)
 
 import numpy as np
@@ -19,35 +18,62 @@ class GAgent(IAgent):
         if chrom is None:
             self.chrom = np.random.rand(GAgent.LEN_CHROM)
         else:
+            assert len(chrom) == GAgent.LEN_CHROM
             self.chrom = chrom
 
 
     def init_red(self):
         return ['A', 'B', 'C', 'D']
-        root = game
         init_place = None
         scores = np.array(self.evaluate(ip) for ip in init_place)
         return init_place[scores.argmax()]
 
+    
+
+    def get_act_afterstates(self, game):
+        nxt_act, _ = self.get_hand(game)
+        return nxt_act
+
+    
+    def get_hand(self, game):
+        self._game = game
+
+        moves = self._game.legalMoves()
+        scores = np.zeros(len(moves))
+
+        for i, nxt in enumerate(moves):
+            game = self._game
+            game.move(*nxt)
+            scores[i] = self.evaluate(game.after_states())
+
+        nxt = scores.argmax()
+        return nxt, moves[nxt]
 
 
-    def evaluate(self):
-        return random.randint(10)
+    def evaluate(self, states):
+        # return board.dot(self.chrom)
+        return np.random.rand()
 
 
 
 if __name__ == '__main__':
     game = Geister2()
 
-    agent1 = GAgent(game)
-    agent2 = GAgent(game)
+    agents = [GAgent(game), GAgent(game)]
     
-    game.printBoard()
-
-    game.setRed(agent1.init_red())
-    game.changeSide()
-    game.setRed(agent2.init_red())
-    game.changeSide()
+    for agent in agents:
+        game.setRed(agent.init_red())
+        game.changeSide()
 
     game.printBoard()
+
+    player = 0
+    while game.checkResult() == 0:
+        states = game.after_states()
+        act = agents[player].get_act_afterstates(states)
+        game.on_action_number_received(act)
+        game.changeSide()
+        player ^= 1
+    game.changeSide()
+    game.printAll()
 
